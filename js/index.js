3e60d2436a6cb15b6fe5f0dfae15807204fd7607
Alessandro <alessandrotokaryev060@gmail.com>
2022-08-08 01:07:13 +0200

    Aggiunto pulsante reset

--- js/index.js
+++ js/index.js
@@ -1,26 +1,28 @@
-//Dichiarazione variabili del contenitore dei numeri (id riferito al codice html) e del valore iniziale del numero da contare
+//Dichiarazione variabili del contenitore dei numeri e del valore iniziale del numero da contare
 let containerNumeri = document.getElementById("numero");
 let valoreIniziale = 0;
 
-
-//Dichiarazione variabili incremento e decremento usate nelle funzioni per assegnare l'azione ai pulsanti +/-
+//Dichiarazione variabili incremento e decremento usate nelle funzioni per assegnare l'azione ai pulsanti +/- & reset
 let incremento = document.querySelector(".inc");
 let decremento = document.querySelector(".dec");
+let resetto = document.querySelector(".res");
 
-
-
-//Funzioni di incremento e decremento
+//Funzioni di incremento, decremento & reset
 function increment(){
     valoreIniziale++;
     containerNumeri.textContent = valoreIniziale;
 }
+
 function decrement(){
     valoreIniziale--;
     containerNumeri.textContent = valoreIniziale;
 }
 
+function reset(){
+    valoreIniziale = 0;
+    containerNumeri.textContent = valoreIniziale;
+}
+
 incremento.addEventListener("click", increment);
 decremento.addEventListener("click", decrement);
-
-
-
+resetto.addEventListener("click", reset);
