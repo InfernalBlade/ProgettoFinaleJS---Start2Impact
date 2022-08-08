05416203d8660b681c7c1eedeaa5c5567120904f
Alessandro <alessandrotokaryev060@gmail.com>
2022-08-08 03:30:59 +0200

    Aggiunta pulsante per contare oppure no numeri negativi

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 //Dichiarazione variabili del contenitore dei numeri e del valore iniziale del numero da contare
+//aggiunto pulsante "impostazioni" dove l'utente decide se contare o no valori minori 0 - di default l'app non lo fa
 let containerNumeri = document.getElementById("numero");
 let valoreIniziale = 0;
+let impValoreMinore = false;
+
 
 //Dichiarazione variabili incremento e decremento usate nelle funzioni per assegnare l'azione ai pulsanti +/- & reset
 let incremento = document.querySelector(".inc");
@@ -14,7 +17,7 @@ function increment(){
 }
 
 function decrement(){
-    if(valoreIniziale===0){
+    if(valoreIniziale===0 && impValoreMinore==false){
         alert("Il valore è 0!")
     }
     else{
@@ -37,3 +40,21 @@ function reset(){
 incremento.addEventListener("click", increment);
 decremento.addEventListener("click", decrement);
 resetto.addEventListener("click", reset);
+
+//Aggiunta funzione di impostazioni, l'utente può ora decidere se contare i numeri negativi oppure no!
+
+let set = document.querySelector(".set");
+function settings(){
+    if(impValoreMinore==true){
+        impValoreMinore=false;
+        set.style.backgroundColor = "";
+        set.style.color = "";
+
+    }else if (impValoreMinore==false){
+        impValoreMinore=true;
+        set.style.backgroundColor = "#cc00ff";
+        set.style.color = "white";
+    }
+
+}
+set.addEventListener("click", settings);
